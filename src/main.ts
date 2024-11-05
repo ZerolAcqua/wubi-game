fc919d91182e41fc66a391ac22d4b40e8074b682
Zerol Acqua <hlc118@outlook.com>
2024-11-05 17:49:00 +0800

    cli: auto on-demand import element-plus

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,8 @@ import { createApp } from 'vue'
 import App from '@/App.vue'
 
 // ElementPlus
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/display.css'
-
 import '@/styles.css';
 
 const app = createApp(App)
-app.use(ElementPlus)
 app.mount('#app')
