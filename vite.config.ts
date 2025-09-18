40e528e6fa2762e41f4ac5e5102535149483699f
ZerolAcqua <hlc118@outlook.com>
2025-09-18 21:28:42 +0800

    feat: github action

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,22 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import ElementPlus from 'unplugin-element-plus/vite'
 
+
+// for github action
+const isGithubAction = process.env.GITHUB_ACTIONS === "true";
+let basePath;
+
+if (isGithubAction) {
+  // 去掉 `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY!.replace(/.*?\//, '');
+  basePath = `/${repo}`
+} else {
+  basePath = '/c/wubi/';
+}
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/c/wubi/',
+  base: basePath,
   plugins: [
     vue(),
     AutoImport({
